84efef0a05e1ecd2a74357215d6232fd492df0c0
gulyaev <gulyaev.gvs@gmail.com>
2021-05-16 16:27:26 +0200

    Authentication

--- authentication.js
+++ authentication.js
@@ -3,6 +3,7 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 const session = require('cookie-session');
+const bodyParser = ('body-parser');
 
 app.use(cookieParser('secret'));
 app.use(session({keys:['secret']}));
@@ -12,6 +13,54 @@ app.use(passport.initialize());
 app.use(passport.session());  
 
 const LocalStrategy = require('passport-local').Strategy;
-passport.use();
+passport.use(new LocalStrategy((username, password, done) => { 
+    if (username !== 'admin') {
+        return done(null, false);
+    }
+
+    if (password !== 'admin') {
+        return done(null, false);
+    }
+
+    return done(null, {firstName: 'Vasya', lastName: 'Pupkin', username: 'admin', id: 1});
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    done(null, {firstName: 'Vasya', lastName: 'Pupkin', username: 'admin', id: 1});
+});
+
+const auth = passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/login'
+});
+
+app.get('/login', (req, res)=>{
+    res.send('TODO: Login form');
+});
+
+app.post('/login', auth);
+
+const mustBeAuthenticated = (req, res, next) => {
+    if(req.isAuthenticaded()) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+app.all('/user', mustBeAuthenticated);
+app.all('/user/*', mustBeAuthenticated);
+
+app.get('/user', (req, res) => {
+    res.send('TODO: User profile');
+});
+
+app.get('/user/settings', (req, res) => {
+    res.send('TODO: User settings');
+});
 
 app.listen(4000);
